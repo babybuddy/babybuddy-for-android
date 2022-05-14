0d3e6c25893c5b4cc621ebd730c29f0724b6085d
Paul Konstantin Gerke <no@localhost.local>
2022-05-14 11:03:08 +0200

    Fix alingment of tutorial arrow

--- app/src/main/java/eu/pkgsoftware/babybuddywidgets/BabyLayoutHolder.java
+++ app/src/main/java/eu/pkgsoftware/babybuddywidgets/BabyLayoutHolder.java
@@ -190,6 +190,8 @@ public class BabyLayoutHolder extends RecyclerView.ViewHolder {
         binding.solidDisabledButton.setVisibility(!changeSolid ? View.VISIBLE : View.GONE);
         binding.wetEnabledButton.setVisibility(changeWet ? View.VISIBLE : View.GONE);
         binding.wetDisabledButton.setVisibility(!changeWet ? View.VISIBLE : View.GONE);
+
+        baseFragment.getMainActivity().tutorialAccess.tutorialMessage(binding.solidDisabledButton, "Hello");
     }
 
     private void storeDiaperChange() {
--- app/src/main/java/eu/pkgsoftware/babybuddywidgets/TutorialAccess.kt
+++ app/src/main/java/eu/pkgsoftware/babybuddywidgets/TutorialAccess.kt
@@ -2,7 +2,10 @@ package eu.pkgsoftware.babybuddywidgets
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Matrix
+import android.graphics.Rect
 import android.view.View
+import android.view.ViewGroup
 
 class TutorialAccess(private val activity: Activity) {
     private val tutorialArrow: View;
@@ -13,7 +16,39 @@ class TutorialAccess(private val activity: Activity) {
         hideTutorial();
     }
 
+    public fun tutorialMessage(view: View, messsage: String) {
+        if (view.visibility != View.VISIBLE) {
+            println("Tutorial: Cannot show tutorial on hidden view");
+            hideTutorial();
+            return;
+        }
+
+        val rect = Rect();
+        view.getGlobalVisibleRect(rect);
+
+        var arrowX = rect.centerX();
+        var arrowY = rect.bottom;
+
+        arrowX = arrowX - tutorialArrow.width / 2;
+        arrowY = arrowY - tutorialArrow.height / 2;
+
+
+        val rootView = tutorialArrow.rootView;
+        val globalRect = Rect()
+        rootView.getGlobalVisibleRect(globalRect);
+
+        arrowX = arrowX - globalRect.left;
+        arrowY = arrowY - globalRect.top;
+
+        tutorialArrow.x = arrowX.toFloat();
+        tutorialArrow.y = arrowY.toFloat();
+
+        tutorialArrow.visibility = View.VISIBLE;
+
+        println("Tutorial: data ${tutorialArrow.width} x ${tutorialArrow.height}");
+    }
+
     public fun hideTutorial() {
-        tutorialArrow.visibility = View.GONE;
+        tutorialArrow.visibility = View.INVISIBLE;
     }
 }
