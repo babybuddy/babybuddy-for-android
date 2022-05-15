a3518b95eec30f4e92c7a0a17a2415f42478bb6c
Paul Konstantin Gerke <no@localhost.local>
2022-05-15 12:43:08 +0200

    Add arrow wiggle

--- app/src/main/java/eu/pkgsoftware/babybuddywidgets/TutorialAccess.kt
+++ app/src/main/java/eu/pkgsoftware/babybuddywidgets/TutorialAccess.kt
@@ -1,19 +1,65 @@
 package eu.pkgsoftware.babybuddywidgets
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.app.Activity
 import android.content.Context
 import android.graphics.Matrix
 import android.graphics.Rect
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.AnimationUtils
+import androidx.core.animation.addListener
 
 class TutorialAccess(private val activity: Activity) {
     private val tutorialArrow: View;
+    private var animations = arrayOf<Animator>();
 
     init {
         tutorialArrow = activity.findViewById(R.id.tutorial_arrow);
 
         hideTutorial();
+    }
+
+    private class AnimStartListener(val startAnim: Animator) : Animator.AnimatorListener {
+        override fun onAnimationStart(p0: Animator?) {
+        }
+
+        override fun onAnimationEnd(p0: Animator?) {
+            startAnim.start();
+        }
+
+        override fun onAnimationCancel(p0: Animator?) {
+        }
+
+        override fun onAnimationRepeat(p0: Animator?) {
+        }
+    };
+
+    private fun stopAnimations() {
+        for (a in animations) {
+            a.cancel();
+        }
+        animations = arrayOf();
+        tutorialArrow.clearAnimation();
+    }
+
+    private fun startArrowAnimation() {
+        val arrowY = tutorialArrow.y.toFloat();
+        val offset = Tools.dpToPx(tutorialArrow.context, 5.0f).toFloat();
+        val a1 = ObjectAnimator.ofFloat(tutorialArrow, "translationY", -offset + arrowY, offset + arrowY).apply {
+            duration = 200;
+        };
+        val a2 = ObjectAnimator.ofFloat(tutorialArrow, "translationY", offset + arrowY, -offset + arrowY).apply {
+            duration = 200;
+        };
+        a1.addListener(AnimStartListener(a2));
+        a2.addListener(AnimStartListener(a1));
+        a1.start();
+
     }
 
     public fun tutorialMessage(view: View, messsage: String) {
@@ -44,6 +90,9 @@ class TutorialAccess(private val activity: Activity) {
         tutorialArrow.y = arrowY.toFloat();
 
         tutorialArrow.visibility = View.VISIBLE;
+
+        stopAnimations();
+        startArrowAnimation();
 
         println("Tutorial: data ${tutorialArrow.width} x ${tutorialArrow.height}");
     }
