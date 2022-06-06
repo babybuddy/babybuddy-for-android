5750fc6041cbca79a6a237d16e055e6ec230ca63
Paul Konstantin Gerke <paulkgerke@craftware.info>
2022-06-06 12:39:09 +0200

    Fix simulated login procedure

--- utils/test-login/test-login-procedure.py
+++ utils/test-login/test-login-procedure.py
@@ -13,7 +13,7 @@ print(json.dumps(dict(initial_response.headers), indent=4))
 
 csrftoken = re.search(
     r"csrftoken=([^;]+);", initial_response.headers["set-cookie"]
-).groups(1)
+).groups(1)[0]
 
 middlewaretoken_input = re.search(
     r'''<input .*name=["']csrfmiddlewaretoken["'][^>]*>''',
@@ -25,16 +25,24 @@ middlewaretoken = re.split('''["']''', middlewaretoken_input.split("value=")[1])
 print("middlewaretoken =", middlewaretoken)
 
 print("=== Login request ===")
-login_request = requests.post(
-    url.rstrip("/") + "/login",
+
+req = requests.Request(
+    "POST",
+    url.rstrip("/") + "/login/",
     data=dict(
         csrfmiddlewaretoken=middlewaretoken,
         username="admin",
         password="admin",
-        next="",
+        next="/",
     ),
     headers={
+        "Referer": initial_response.url,
         "Cookie": f"csrftoken={csrftoken}",
     }
 )
+prepped_req = req.prepare()
+print("prepped_req", prepped_req.headers, prepped_req.body)
+
+s = requests.Session()
+login_request = s.send(prepped_req)
 print("code =", login_request.status_code)
