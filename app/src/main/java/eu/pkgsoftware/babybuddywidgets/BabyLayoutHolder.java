47d3cfb603f0a0bdc34e04244f413058f8e43f4a
Paul Konstantin Gerke <paulkgerke@craftware.info>
2023-05-13 00:23:14 +0200

    Extract internal class from BabyLayoutHolder

--- app/src/main/java/eu/pkgsoftware/babybuddywidgets/BabyLayoutHolder.java
+++ app/src/main/java/eu/pkgsoftware/babybuddywidgets/BabyLayoutHolder.java
@@ -1,132 +1,23 @@
 package eu.pkgsoftware.babybuddywidgets;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import eu.pkgsoftware.babybuddywidgets.databinding.BabyManagerBinding;
 import eu.pkgsoftware.babybuddywidgets.databinding.NotesEditorBinding;
-import eu.pkgsoftware.babybuddywidgets.databinding.QuickTimerEntryBinding;
 import eu.pkgsoftware.babybuddywidgets.history.ChildEventHistoryLoader;
 import eu.pkgsoftware.babybuddywidgets.networking.BabyBuddyClient;
 import eu.pkgsoftware.babybuddywidgets.networking.ChildrenStateTracker;
-import eu.pkgsoftware.babybuddywidgets.timers.TimerListViewHolder;
-import eu.pkgsoftware.babybuddywidgets.timers.TimerListViewHolderCallback;
+import eu.pkgsoftware.babybuddywidgets.timers.TimerListProvider;
 import eu.pkgsoftware.babybuddywidgets.widgets.SwitchButtonLogic;
 
 public class BabyLayoutHolder extends RecyclerView.ViewHolder {
-    private class TimerListProvider extends RecyclerView.Adapter<TimerListViewHolder> implements TimerListViewHolderCallback {
-        private BabyBuddyClient.Timer[] timers = new BabyBuddyClient.Timer[0];
-        private List<TimerListViewHolder> holders = new LinkedList<>();
-
-        private int[] listIds(BabyBuddyClient.Timer[] t) {
-            int[] result = new int[t.length];
-            for (int i = 0; i < t.length; i++) {
-                result[i] = t[i].id;
-            }
-            Arrays.sort(result);
-            return result;
-        }
-
-        private boolean compareIds(BabyBuddyClient.Timer[] t1, BabyBuddyClient.Timer[] t2) {
-            return Arrays.equals(listIds(t1), listIds(t2));
-        }
-
-        private TimerListViewHolder findHolderForTimer(BabyBuddyClient.Timer t) {
-            TimerListViewHolder result = null;
-            for (TimerListViewHolder h : holders) {
-                if (h.getTimer().id == t.id) {
-                    if (result != null) {
-                        return null; // Multiple timers - dismiss
-                    } else {
-                        result = h;
-                    }
-                }
-            }
-            return result;
-        }
-
-        public void updateTimers(BabyBuddyClient.Timer[] timers) {
-            timers = timers.clone();
-            Arrays.sort(timers, (a, b) -> Integer.compare(a.id, b.id));
-
-            if (!compareIds(timers, this.timers)) {
-                this.timers = timers;
-                notifyDataSetChanged();
-            } else {
-                for (int i = 0; i < timers.length; i++) {
-                    if (!this.timers[i].equals(timers[i])) {
-                        BabyBuddyClient.Timer probeTimer = timers[i].clone();
-                        probeTimer.start = this.timers[i].start;
-                        probeTimer.end = this.timers[i].end;
-                        TimerListViewHolder timerHolder = findHolderForTimer(timers[i]);
-                        boolean probeTimerEqual = probeTimer.equals(this.timers[i]);
-
-                        this.timers[i] = timers[i];
-                        if (probeTimerEqual && (timerHolder != null)) {
-                            timerHolder.assignTimer(timers[i]);
-                        } else {
-                            notifyItemChanged(i);
-                        }
-                    }
-                }
-            }
-        }
-
-        @Override
-        public TimerListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            QuickTimerEntryBinding entryBinding = QuickTimerEntryBinding.inflate(LayoutInflater.from(parent.getContext()));
-            return new TimerListViewHolder(baseFragment, entryBinding, this);
-        }
-
-        @Override
-        public void onViewAttachedToWindow(@NonNull TimerListViewHolder holder) {
-            super.onViewAttachedToWindow(holder);
-            holders.add(holder);
-        }
-
-        @Override
-        public void onViewDetachedFromWindow(@NonNull TimerListViewHolder holder) {
-            super.onViewDetachedFromWindow(holder);
-            holders.remove(holder);
-        }
-
-        @Override
-        public void onBindViewHolder(TimerListViewHolder holder, int position) {
-            holder.assignTimer(timers[position]);
-        }
-
-        @Override
-        public int getItemCount() {
-            return timers.length;
-        }
-
-        public void close() {
-            for (TimerListViewHolder h : holders) {
-                h.close();
-            }
-        }
-
-        @Override
-        public void updateActivities() {
-            if (childHistoryLoader != null) {
-                childHistoryLoader.forceRefresh();
-            }
-        }
-    }
-
-    private BabyManagerBinding binding;
+    private final BabyManagerBinding binding;
     private final BaseFragment baseFragment;
     private final BabyBuddyClient client;
     private final CredStore credStore;
-    private BabyLayoutHolder.TimerListProvider timerListProvider;
+    private TimerListProvider timerListProvider = null;
 
     private BabyBuddyClient.Child child = null;
 
@@ -171,9 +62,6 @@ public class BabyLayoutHolder extends RecyclerView.ViewHolder {
             binding.removeNoteButton,
             false
         );
-
-        timerListProvider = new BabyLayoutHolder.TimerListProvider();
-        binding.timersList.setAdapter(timerListProvider);
 
         NotesEditorBinding notesEditorBinding = NotesEditorBinding.inflate(
             fragment.getMainActivity().getLayoutInflater()
@@ -351,6 +239,7 @@ public class BabyLayoutHolder extends RecyclerView.ViewHolder {
             childHistoryLoader.close();
         }
         childHistoryLoader = null;
+        binding.timersList.setAdapter(null);
     }
 
     public void updateChild(BabyBuddyClient.Child c, ChildrenStateTracker stateTracker) {
@@ -372,6 +261,8 @@ public class BabyLayoutHolder extends RecyclerView.ViewHolder {
                 binding.timelineProgressSpinner
             );
             childHistoryLoader.createTimelineObserver(stateTracker);
+            timerListProvider = new TimerListProvider(baseFragment, childHistoryLoader);
+            binding.timersList.setAdapter(timerListProvider);
         }
     }
 
