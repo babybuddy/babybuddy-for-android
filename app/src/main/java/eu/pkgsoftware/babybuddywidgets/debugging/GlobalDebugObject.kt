b82c4900074d9f528fa2d7af8d200d0d5dc85cc7
Paul Konstantin Gerke <paulkgerke@craftware.info>
2023-07-25 22:58:53 +0200

    Implement debugging inside a dedicated class for debugging

---
+++ app/src/main/java/eu/pkgsoftware/babybuddywidgets/debugging/GlobalDebugObject.kt
@@ -0,0 +1,38 @@
+package eu.pkgsoftware.babybuddywidgets.debugging
+
+import android.util.Log
+
+class GlobalDebugObject {
+    companion object {
+        val ENABLED = true
+        val DO_PRINT = true
+        val LOG_FILE_MESSAGE_LIMIT = 10000
+
+        val LOCK = Object()
+
+        private val log = mutableListOf<String>()
+
+        @JvmStatic
+        fun log(msg: String) {
+            if (!ENABLED) return
+            synchronized(LOCK) {
+                while (log.size > LOG_FILE_MESSAGE_LIMIT) {
+                    log.removeAt(0)
+                }
+                if (DO_PRINT) {
+                    Log.println(Log.DEBUG, "GlobalDebugObject.log", msg)
+                }
+                log.add(msg)
+            }
+        }
+
+        @JvmStatic
+        fun getLog(): List<String> {
+            if (!ENABLED) return listOf()
+            val result = synchronized(LOCK) {
+                log.toList()
+            }
+            return result
+        }
+    }
+}
