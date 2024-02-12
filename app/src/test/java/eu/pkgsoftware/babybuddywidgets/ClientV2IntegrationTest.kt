50d97f8f8fd68240cb51464956006412a4eb2223
Paul Konstantin Gerke <paulkgerke@craftware.info>
2024-02-13 00:40:56 +0100

    Add parameterized test for getting list items

--- app/src/test/java/eu/pkgsoftware/babybuddywidgets/ClientV2IntegrationTest.kt
+++ app/src/test/java/eu/pkgsoftware/babybuddywidgets/ClientV2IntegrationTest.kt
@@ -21,16 +21,29 @@ import org.junit.Test
 import java.io.File
 import java.net.URL
 
+data class ServerUrl(val version: String, val url: String)
+
 class ClientV2IntegrationTest {
-    fun serverUrl(): String {
-        return System.getProperty(
-            "babybuddy.testserver",
-            "http://localhost:9006"
-        )!!
+    var serverUrlArray = mutableListOf<ServerUrl>()
+
+    init {
+        val path = File("../tools/babybuddy-version-array/versions")
+        var portIndex = 9000
+        for (l in path.readLines()) {
+            val line = l.trim()
+            if (line.length == 0) continue
+            if (line.startsWith("#")) continue
+            serverUrlArray.add(
+                ServerUrl(line, "http://localhost:$portIndex")
+            )
+            portIndex += 1
+        }
     }
 
+    var serverUrl = ""
+
     inner class AppInterface : ServerAccessProviderInterface {
-        private val _serverUrl = this@ClientV2IntegrationTest.serverUrl()
+        private val _serverUrl = this@ClientV2IntegrationTest.serverUrl
         private var _appToken = ""
 
         init {
@@ -53,8 +66,7 @@ class ClientV2IntegrationTest {
         }
     }
 
-    @Test
-    fun testObtainLists() = runTest {
+    suspend fun runObtainListsTest() {
         val client = Client(AppInterface())
 
         println("Get profile")
@@ -114,5 +126,14 @@ class ClientV2IntegrationTest {
         val headCircEntires = client.getEntries(HeadCircumferenceEntry::class, 0, 100)
         Assert.assertTrue(headCircEntires.totalCount > 0)
         Assert.assertTrue(headCircEntires.entries.size > 0)
+    }
+
+    @Test
+    fun testObtainLists() = runTest {
+        for (server in serverUrlArray) {
+            serverUrl = server.url
+            println("TESTING BABYBUDDY VERSION: ${server.version}")
+            runObtainListsTest()
+        }
     }
 }
