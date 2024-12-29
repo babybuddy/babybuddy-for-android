acbe84dfd8783187a52ee9fd5a78f9c8c71e3e5b
Paul Konstantin Gerke <paulkgerke@craftware.info>
2024-12-30 00:39:30 +0100

    Unify implementation style of application menus

--- app/src/main/java/eu/pkgsoftware/babybuddywidgets/login/LoggedInMenu.kt
+++ app/src/main/java/eu/pkgsoftware/babybuddywidgets/login/LoggedInMenu.kt
@@ -1,6 +1,5 @@
 package eu.pkgsoftware.babybuddywidgets.login
 
-import androidx.navigation.Navigation
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
@@ -19,25 +18,22 @@ class LoggedInMenu(val fragment: BaseFragment) : MenuProvider {
     }
 
     override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        val view = fragment.view
-        if (view == null) {
-            return false
-        }
+        val view = fragment.view ?: return false
         val navController = findNavController(view)
-        if (menuItem.getItemId() == R.id.logoutMenuButton) {
+        if (menuItem.itemId == R.id.logoutMenuButton) {
             fragment.mainActivity.logout()
             navController.navigate(R.id.logoutOperation)
         }
-        if (menuItem.getItemId() == R.id.showHelpMenuButton) {
+        if (menuItem.itemId == R.id.showHelpMenuButton) {
             navController.navigate(R.id.global_showHelp)
         }
-        if (menuItem.getItemId() == R.id.aboutPageMenuItem) {
+        if (menuItem.itemId == R.id.aboutPageMenuItem) {
             navController.navigate(R.id.global_aboutFragment)
         }
-        if (menuItem.getItemId() == R.id.exportDebugLogsMenuItem) {
+        if (menuItem.itemId == R.id.exportDebugLogsMenuItem) {
             navController.navigate(R.id.action_global_debugLogDisplay)
         }
-        if (menuItem.getItemId() == R.id.contactDeveloperMenuItem) {
+        if (menuItem.itemId == R.id.contactDeveloperMenuItem) {
             navController.navigate(R.id.action_global_contactDeveloperFragment)
         }
         return false
